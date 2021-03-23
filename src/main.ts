4dbf7bd378b21050dd913bb11d6a700b54075951
TINGCYGF <1127163160@qq.com>
2021-03-23 17:30:45 +0800

    构建基础页面结构

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createWebHashHistory, createRouter } from 'vue-router'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
-createApp(App).mount('#app')
+
+const history = createWebHashHistory()
+const router = createRouter({
+  history: history,
+  routes: [
+    {path: '/', component: Home},
+    {path: '/doc', component: Doc}
+
+  ]
+})
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
---
+++ src/views/Doc.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="content">
+      <aside>边栏</aside>
+      <main>主体</main>
+    </div>
+  </div>
+</template>
