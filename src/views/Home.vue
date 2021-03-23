f6848b331c2d1e4f95dc5283daea909c4344b96a
TINGCYGF <1127163160@qq.com>
2021-03-23 17:51:59 +0800

    topnav组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
+    <Topnav/>
     <div class="banner">
       <h1>Ting UI</h1>
       <h2>一个厉害的 UI 框架</h2>
@@ -14,3 +11,11 @@
     </div>
   </div>
 </template>
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: {Topnav}
+}
+</script>
+
+<style lang="scss" scoped></style>
